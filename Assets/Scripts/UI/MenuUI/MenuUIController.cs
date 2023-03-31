8c21b3737fbc3d1f3b7b3b7a9df12da53718450f
Ömer Ertekin <57711950+OmerErtekin@users.noreply.github.com>
2023-03-31 15:30:34 +0300

    Hide levels button

--- Assets/Scripts/UI/MenuUI/MenuUIController.cs
+++ Assets/Scripts/UI/MenuUI/MenuUIController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ public class MenuUIController : MonoBehaviour
 {
     #region Components
     public static MenuUIController Instance;
+    [SerializeField] private CanvasGroup levelsButton;
     [SerializeField] private LevelViewer levelViewer;
     [SerializeField] private Celebration celebration;
     #endregion
@@ -17,7 +19,7 @@ public class MenuUIController : MonoBehaviour
 
     private void Start()
     {
-        if (PlayerPrefs.GetInt("DidComeFromGame", 1) == 1)
+        if (PlayerPrefs.GetInt("DidComeFromGame", 0) == 1)
         {
             IngameLoadingScreen.Instance.HideLoadingScreen(true);
             StartCoroutine(CameFromMenuRoutine());
@@ -31,19 +33,29 @@ public class MenuUIController : MonoBehaviour
 
     private IEnumerator CameFromMenuRoutine()
     {
-        yield return new WaitForSeconds(1);
-        if (PlayerPrefs.GetInt("HasNewHighScore", 1) == 1)
+        levelsButton.gameObject.SetActive(false);
+        if (PlayerPrefs.GetInt("HasNewHighScore", 0) == 1)
         {
+            yield return new WaitForSeconds(1);
             celebration.CelebrateHighestScore();
         }
         else
         {
+            yield return new WaitForSeconds(1);
             ShowLevelViewer(false);
         }
     }
 
     public void ShowLevelViewer(bool willUnlockNextLevel)
     {
+        levelsButton.DOFade(0, 0.5f).SetTarget(this);
         levelViewer.ShowLevelViewer(willUnlockNextLevel);
+    }
+
+    public void HideLevelViewer()
+    {
+        levelsButton.gameObject.SetActive(true);
+        levelsButton.DOFade(1, 0.5f).SetTarget(this);
+        levelViewer.HideLevelViewer();
     }
 }
